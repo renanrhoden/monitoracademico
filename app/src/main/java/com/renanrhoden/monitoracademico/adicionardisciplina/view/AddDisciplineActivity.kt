80e79fa18fe7a8bcc90b505fec12e73e90cfd460
Renan Rhoden <renan.rhoden@ilegra.com>
2018-05-25 19:40:01 -0300

    scheduling

--- app/src/main/java/com/renanrhoden/monitoracademico/adicionardisciplina/view/AddDisciplineActivity.kt
+++ app/src/main/java/com/renanrhoden/monitoracademico/adicionardisciplina/view/AddDisciplineActivity.kt
@@ -1,5 +1,6 @@
 package com.renanrhoden.monitoracademico.adicionardisciplina.view
 
+import android.content.Context
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -8,11 +9,15 @@ import com.renanrhoden.monitoracademico.adicionardisciplina.viewmodel.AddDiscipl
 import com.renanrhoden.monitoracademico.databinding.AddDisciplineActivityBinding
 
 
+
+
 class AddDisciplineActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<AddDisciplineActivityBinding>(this, R.layout.add_discipline_activity)
         binding.viewModel = AddDisciplineViewModel()
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE)
     }
 }
--- app/src/main/java/com/renanrhoden/monitoracademico/adicionardisciplina/viewmodel/AddDisciplineViewModel.kt
+++ app/src/main/java/com/renanrhoden/monitoracademico/adicionardisciplina/viewmodel/AddDisciplineViewModel.kt
@@ -2,6 +2,7 @@ package com.renanrhoden.monitoracademico.adicionardisciplina.viewmodel
 
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
+import io.hypertrack.smart_scheduler.SmartScheduler
 
 class AddDisciplineViewModel {
     val mNameDiscipline = ObservableField<String>("")
@@ -12,4 +13,22 @@ class AddDisciplineViewModel {
     val mSextaChecked = ObservableBoolean(false)
     val mSabadoChecked = ObservableBoolean(false)
     val mDomingoChecked = ObservableBoolean(false)
+
+    val callback = SmartScheduler.JobScheduledCallback { context, job ->
+
+    }
+//
+//    fun createJob(){
+//        val builder = Job.Builder(1, callback, Job.Type.JOB_TYPE_ALARM, "tag")
+//                .
+//    }
+//    .setRequiredNetworkType(networkType)
+//    .setRequiresCharging(requiresCharging)
+//    .setIntervalMillis(intervalInMillis);
+//
+//    if (isPeriodic) {
+//        builder.setPeriodic(intervalInMillis);
+//    }
+//
+//    Job job = builder.build();
 }
